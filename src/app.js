6f994b8670485c6e0aa1a75f165cdb13df2872d5
Jessmor2 <135068935+Jessmor2@users.noreply.github.com>
2023-07-19 18:14:33 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,39 +1,125 @@
 /* eslint-disable */
-
-let suits = ["♠", "♥", "♦", "♣"];
-let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-
-let generateSuit = () => {
-  let randomSuit = Math.floor(Math.random() * suits.length);
-  return suits[randomSuit];
-};
-
-let generateValue = () => {
-  let randomValue = Math.floor(Math.random() * values.length);
-  return values[randomValue];
-};
-
-var divElem = document.getElementById("playingCard");
-var firstH2 = document.createElement("h2");
-var cardSuit = document.createTextNode(`${generateSuit()}`);
-firstH2.appendChild(cardSuit);
-divElem.appendChild(firstH2);
-var firstH1 = document.createElement("h1");
-var cardValue = document.createTextNode(`${generateValue()}`);
-firstH1.appendChild(cardValue);
-divElem.appendChild(firstH1);
-var secondH2 = document.createElement("h2");
-var bottomSuit = document.createTextNode(`${cardSuit}`);
-secondH2.appendChild(bottomSuit);
-divElem.appendChild(secondH2);
-
-//document.querySelector("#playingCard").innerHTML = `<div>
-//<h2>${generateSuit()}</h2>
-//<h1>${generateValue()}</h1>
-//<h2>${generateSuit()}</h2>
-//</div>`;
+import "./style.css";
 
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console");
+  let suits = ["♠", "♥", "♦", "♣"];
+  let values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let generateSuit = () => {
+    let randomSuit = Math.floor(Math.random() * suits.length);
+    return suits[randomSuit];
+  };
+  const newSuit = generateSuit();
+
+  let generateValue = () => {
+    let randomValue = Math.floor(Math.random() * values.length);
+    return values[randomValue];
+  };
+  const newValue = generateValue();
+
+  let getTopNumber = (document.querySelector(
+    "#topNumber"
+  ).innerHTML = newValue);
+  let getSuitTop = (document.querySelector("#topSuit").innerHTML = newSuit);
+  let getcenterNumber = (document.querySelector(
+    "#centerNumber"
+  ).innerHTML = newValue);
+  let getSuitBottom = (document.querySelector(
+    "#bottomSuit"
+  ).innerHTML = newSuit);
+  let getbottomNumber = (document.querySelector(
+    "#bottomNumber"
+  ).innerHTML = newValue);
+
+  if (newSuit === "♥" || newSuit === "♦") {
+    document.getElementById("topSuit").style.color = "red";
+    document.getElementById("bottomSuit").style.color = "red";
+    document.getElementById("topNumber").style.color = "red";
+    document.getElementById("bottomNumber").style.color = "red";
+    document.getElementById("centerNumber").style.color = "red";
+  } else {
+    document.getElementById("topSuit").style.color = "black";
+    document.getElementById("bottomSuit").style.color = "black";
+    document.getElementById("topNumber").style.color = "black";
+    document.getElementById("bottomNumber").style.color = "black";
+    document.getElementById("centerNumber").style.color = "black";
+  }
 };
+
+function drawNewCard() {
+  let suits = ["♠", "♥", "♦", "♣"];
+  let values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let generateSuit = () => {
+    let randomSuit = Math.floor(Math.random() * suits.length);
+    return suits[randomSuit];
+  };
+  const newSuit = generateSuit();
+
+  let generateValue = () => {
+    let randomValue = Math.floor(Math.random() * values.length);
+    return values[randomValue];
+  };
+  const newValue = generateValue();
+
+  let getTopNumber = (document.querySelector(
+    "#topNumber"
+  ).innerHTML = newValue);
+  let getSuitTop = (document.querySelector("#topSuit").innerHTML = newSuit);
+  let getcenterNumber = (document.querySelector(
+    "#centerNumber"
+  ).innerHTML = newValue);
+  let getSuitBottom = (document.querySelector(
+    "#bottomSuit"
+  ).innerHTML = newSuit);
+  let getbottomNumber = (document.querySelector(
+    "#bottomNumber"
+  ).innerHTML = newValue);
+
+  if (newSuit === "♥" || newSuit === "♦") {
+    document.getElementById("topSuit").style.color = "red";
+    document.getElementById("bottomSuit").style.color = "red";
+    document.getElementById("topNumber").style.color = "red";
+    document.getElementById("bottomNumber").style.color = "red";
+    document.getElementById("centerNumber").style.color = "red";
+  } else {
+    document.getElementById("topSuit").style.color = "black";
+    document.getElementById("bottomSuit").style.color = "black";
+    document.getElementById("topNumber").style.color = "black";
+    document.getElementById("bottomNumber").style.color = "black";
+    document.getElementById("centerNumber").style.color = "black";
+  }
+}
+
+const btn = document.querySelector("#button1");
+btn.addEventListener("click", drawNewCard);
+
+setInterval(drawNewCard, 10000);
